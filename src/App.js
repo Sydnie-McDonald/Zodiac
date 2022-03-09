94b027ac0bd352e36a812f1596ad3db11e9daa16
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-03-08 19:18:43 -0600

    adding background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import background from '../background.png';
 
 function App() {
   return (
+  <main className="main" style={{ backgroundImage: `url(${background})` }}></main>
     <div className="App">
-      <header className="App-header">
+      <header className="App-header"></header>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
     </div>
   );
 }
